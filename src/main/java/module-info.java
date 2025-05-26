333d7da1941a177bb961d454d17818ddba3dd518
Daniel Sánchez Vargas <danielpat433@gmail.com>
2025-05-25 20:50:06 -0600

    limpieza

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.imaginationholdingsclient {
     requires javafx.controls;
     requires javafx.fxml;
+    requires imaginationHoldingsLibrary;
 
 
     opens com.imaginationholdingsclient to javafx.fxml;
     exports com.imaginationholdingsclient;
+    opens controller to javafx.fxml;
+    exports controller;
 }
